bd01dbdeecce618aa35c65eda572e3d8386b0373
DSpro1 <danielverissimo1d@gmail.com>
2024-05-16 20:41:42 -0300

    adição da tela de salão - faltandajuste na adição da foto de logo

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Login } from '../pages/Login/Login.jsx';
 import { Cadastro } from '../pages/Register/Cadastro.jsx';
 import { HomeHeader } from '../pages/Inicial/Home.jsx';
+import { Salon } from '../pages/Salon/cad-salon.jsx';
 
 export const AppRouter = () => {
     return (
@@ -10,6 +11,7 @@ export const AppRouter = () => {
                 <Route path="/" element={<Login />} /> 
                 <Route path="/cadastro" element={<Cadastro />} />
                 <Route path="/home" element={<HomeHeader />} />
+                <Route path="/cad-salon" element={<Salon />}/>
             </Routes>
         </Router>
     );
