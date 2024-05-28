97cbbb28b4113897e6068e7bd97b3caa5f271c97
JasonCris4 <jasonalves444@gmail.com>
2024-05-27 21:10:10 -0300

    Rota barbeiros(com bug)

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,6 +4,7 @@ import  Cadastro  from '../pages/Register/index.jsx';
 import  HomeHeader  from '../pages/Inicial/index.jsx';
 import  Salon  from '../pages/Salon/index.jsx';
 import ForgotPassword from '../pages/Forgotpass/forgot.jsx';
+import AddBarber from '../pages/Barbeiros/barber.jsx';
 
 function AppRouter () {
     return (
@@ -14,9 +15,10 @@ function AppRouter () {
                 <Route path="/home" element={ <HomeHeader /> }></Route>
                 <Route path="/cad-salon" element={ <Salon /> }></Route>
                 <Route path="/forgot-password" element={ <ForgotPassword /> }></Route>
+                <Route path="/add-barber" element={ <AddBarber />}></Route>
             </Routes>
         </Router>
     );
-}
+}0
 
 export default AppRouter;
